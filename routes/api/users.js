44d30cf5929ab3b2b2ea8a8c33bf3b5e34c23a6a
David Dinh <dmdinh22@gmail.com>
2018-06-08 00:46:50 -0700

    fixing merge conflicts

---
+++ routes/api/users.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// load user model
+const User = require('../../models/User');
+
+// @route   GET api/users/test
+// @desc    tests users route
+// @access  public
+router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
+
+// @route   POST api/users/register
+// @desc    register user
+// @access  public
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            return res.status(400).json({ email: 'Email already registered.' });
+        } else {
+            const avatar = gravatar.url(req.body.email, {
+                s: '200', // size
+                r: 'r', //rating
+                d: 'mm' //default
+            });
+
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                avatar,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+});
+
+// @route   POST api/users/login
+// @desc    log in user / return JWT token
+// @access  public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.psasword;
+
+    // find user by email
+    User.findOne({ email }).then(user => {
+        if (!user) {
+            return res.status(404).json({ email: 'User not found' });
+        }
+
+        // check pw match using bcrypt
+        bcrypt
+            .compare(password, user.password) // password from request, password from user in db
+            .then(isMatch => {
+                if (isMatch) {
+                    res.json({ msg: 'Success' });
+                } else {
+                    return res
+                        .status(400)
+                        .json({ password: 'Password incorrect.' });
+                }
+            });
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -13,6 +22,11 @@ mongoose
     .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello world.'));
+
+// routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
