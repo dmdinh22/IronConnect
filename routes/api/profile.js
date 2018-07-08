de8522eaff67d73a4c64cd0b4223e1686f20a889
David Dinh <dmdinh22@gmail.com>
2018-07-08 02:48:25 -0700

    get user by user id endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -49,11 +49,36 @@ router.get(
 // @route   GET api/profile/handle/:handle
 // @desc    Get profile by handle
 // @access  Public
-
 router.get('/handle/:handle', (req, res) => {
     const errors = {};
     Profile.findOne({
         handle: req.params.handle
+    })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user.';
+                res.status(404).json(errors);
+            }
+
+            res.json(profile);
+        })
+        .catch(error =>
+            res.status(404)
+                .json({
+                    profile: 'There is no profile for this user.',
+                    error: error
+                })
+        );
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', (req, res) => {
+    const errors = {};
+    Profile.findOne({
+        user: req.params.user_id
     })
         .populate('user', ['name', 'avatar'])
         .then(profile => {
