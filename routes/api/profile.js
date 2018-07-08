e1880c6f65e788d32546ae0257dd2c610ae3383e
David Dinh <dmdinh22@gmail.com>
2018-07-08 03:12:45 -0700

    remove user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -277,5 +277,31 @@ router.delete(
     }
 );
 
+// @route   DELETE api/profile/
+// @desc    Delete user and profile
+// @access  Private
+router.delete(
+    '/',
+    passport.authenticate('jwt', {
+        session: false
+    }),
+    (req, result) => {
+    // deletes profile
+        Profile.findOneAndRemove({
+            user: req.user.id
+        })
+            .then(() => {
+                // deletes user
+                User.findOneAndRemove({
+                    _id: req.user.id
+                })
+                    .then(() => {
+                        result.json({
+                            success: true
+                        });
+                    });
+            });
+    }
+);
 
 module.exports = router;
