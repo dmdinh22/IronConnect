8e979f180c753e7c5da24a7c2f239f96c9ea7c27
David Dinh <dmdinh22@gmail.com>
2018-07-08 02:53:16 -0700

    get all profiles endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -46,6 +46,31 @@ router.get(
     }
 );
 
+// @route   GET api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get('/all', (req, res) => {
+    const errors = {};
+
+    Profile.find()
+        .populate('user', ['name', 'avatar'])
+        .then(profiles => {
+            if (!profiles) {
+                errors.noprofile = 'There are no profiles.';
+                return res.status(404).json(errors);
+            }
+
+            res.json(profiles);
+        })
+        .catch(error =>
+            res.status(404)
+                .json({
+                    profile: 'There are no profiles.',
+                    error: error
+                })
+        );
+});
+
 // @route   GET api/profile/handle/:handle
 // @desc    Get profile by handle
 // @access  Public
