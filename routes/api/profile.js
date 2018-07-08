799fbf8fa03dfa1e7497f323c1079d493aede3b9
David Dinh <dmdinh22@gmail.com>
2018-07-08 02:44:57 -0700

    get user by handle endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -46,6 +46,33 @@ router.get(
     }
 );
 
+// @route   GET api/profile/handle/:handle
+// @desc    Get profile by handle
+// @access  Public
+
+router.get('/handle/:handle', (req, res) => {
+    const errors = {};
+    Profile.findOne({
+        handle: req.params.handle
+    })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user.';
+                res.status(404).json(errors);
+            }
+
+            res.json(profile);
+        })
+        .catch(error =>
+            res.status(404)
+                .json({
+                    profile: 'There is no profile for this user.',
+                    error: error
+                })
+        );
+});
+
 // @route   POST api/profile
 // @desc    Create/edit user profile
 // @access  Private
