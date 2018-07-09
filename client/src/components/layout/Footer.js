4e99bf68065527f83a061b8b68ba6d1f6c264931
David Dinh <dmdinh22@gmail.com>
2018-07-09 01:38:10 -0700

    add onChange function

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,9 +2,9 @@
 import React from 'react';
 
 export default () => {
-	return (
-		<footer className="bg-dark text-white mt-5 p-4 text-center">
-			Copyright &copy; {new Date().getFullYear()} IronConnector
-		</footer>
-	);
+    return (
+        <footer className="bg-dark text-white mt-5 p-4 text-center">
+            Copyright &copy; {new Date().getFullYear()} IronConnector
+        </footer>
+    );
 };
