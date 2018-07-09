bdc49a6debe36419eeebda114dfae7221ce33cd2
David Dinh <dmdinh22@gmail.com>
2018-07-09 01:40:17 -0700

    wire up submit

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -13,12 +13,27 @@ class Register extends Component {
 
         // binding the functions to 'this'
         this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     onChange(e) {
         this.setState({
             [e.target.name]: e.target.value
         });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        // create new user object
+        const newUser = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            password2: this.state.password2
+        };
+
+        console.log(newUser);
     }
 
     render() {
@@ -31,7 +46,7 @@ class Register extends Component {
                             <p className="lead text-center">
                                 Create your DevsConnect account
                             </p>
-                            <form action="create-profile.html">
+                            <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
                                     <input
                                         type="text"
