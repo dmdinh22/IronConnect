b4196cad6e9fccf7a1680805971281bc1415fc91
David Dinh <dmdinh22@gmail.com>
2018-08-30 00:18:12 -0700

    add auth action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 
 class Register extends Component {
     constructor() {
@@ -35,12 +37,14 @@ class Register extends Component {
             password2: this.state.password2
         };
 
-        // hit api endpoint for testing - will be done in redux later
-        axios
-            .post('/api/users/register', newUser)
-            .then(res => console.log(res.data))
-            // set state of error object with response data
-            .catch(err => this.setState({ errors: err.response.data }));
+        this.props.registerUser(newUser);
+
+        // // hit api endpoint for testing - will be done in redux later
+        // axios
+        //     .post('/api/users/register', newUser)
+        //     .then(res => console.log(res.data))
+        //     // set state of error object with response data
+        //     .catch(err => this.setState({ errors: err.response.data }));
     }
 
     render() {
@@ -150,4 +154,7 @@ class Register extends Component {
     }
 }
 
-export default Register;
+export default connect(
+    null,
+    { registerUser }
+)(Register);
