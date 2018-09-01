bc147ddb6ef06b72aa9ec922a596f9c273a6b65a
David Dinh <dmdinh22@gmail.com>
2018-09-01 00:14:38 -0700

    set up loginUser action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
+
 import { GET_ERRORS } from './types';
 
 // register user
@@ -8,6 +11,31 @@ export const registerUser = (userData, history) => dispatch => {
         .post('/api/users/register', userData)
         .then(res => history.push('/login'))
         // set state of error object with response data
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
+
+// login - get User Token
+export const loginUser = userData => dispatch => {
+    axios
+        .post('/api/users/login', userData)
+        .then(res => {
+            // save to localStorage
+            const { token } = res.data;
+
+            // set token to localStorage
+            localStorage.setItem('jwtToken', token);
+
+            // set token to Auth header
+            setAuthToken(token);
+
+            // decode token to get user data
+            const decoded = jwt_decode(token);
+        })
         .catch(err =>
             dispatch({
                 type: GET_ERRORS,
