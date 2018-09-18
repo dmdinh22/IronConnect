a661df23b82611f75f44c4621a3a0a2514460a47
David Dinh <dmdinh22@gmail.com>
2018-09-18 00:24:22 -0700

    wire up onSubmit action

--- client/src/components/create_profile/CreateProfile.js
+++ client/src/components/create_profile/CreateProfile.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import InputGroup from '../common/InputGroup';
 import SelectListGroup from '../common/SelectListGroup';
+import { createProfile } from '../../actions/profileActions';
 
 class CreateProfile extends Component {
     constructor(props) {
@@ -16,6 +18,7 @@ class CreateProfile extends Component {
             skills: '',
             skilllevel: '',
             bio: '',
+            website: '',
             twitter: '',
             facebook: '',
             linkedin: '',
@@ -29,10 +32,32 @@ class CreateProfile extends Component {
         this.onSubmit = this.onSubmit.bind(this);
     }
 
+    // lifecycle method
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({ errors: nextProps.errors });
+        }
+    }
+
     onSubmit(e) {
         e.preventDefault();
 
-        console.log('submitting...');
+        const profileData = {
+            handle: this.state.handle,
+            location: this.state.location,
+            sport: this.state.sport,
+            skills: this.state.skills,
+            skilllevel: this.state.skilllevel,
+            bio: this.state.bio,
+            website: this.state.website,
+            twitter: this.state.twitter,
+            facebook: this.state.facebook,
+            linkedin: this.state.linkedin,
+            youtube: this.state.youtube,
+            instagram: this.state.instagram
+        };
+
+        this.props.createProfile(profileData, this.props.history);
     }
 
     onChange(e) {
@@ -230,4 +255,7 @@ const mapStateToProps = state => ({
     errors: state.errors
 });
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(
+    mapStateToProps,
+    { createProfile }
+)(withRouter(CreateProfile));
