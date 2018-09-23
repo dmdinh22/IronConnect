ca661876a05bfd8fe5c0a76745f8d22be3dc9bb1
David Dinh <dmdinh22@gmail.com>
2018-09-23 00:47:50 -0700

    Add field inputs for Add Meets form

--- client/src/components/add-meets/AddMeets.js
+++ client/src/components/add-meets/AddMeets.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import { TextFieldGroup } from '../common/TextFieldGroup';
-import { TextAreaFieldGroup } from '../common/TextAreaFieldGroup';
+import TextFieldGroup from '../common/TextFieldGroup';
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -39,9 +39,68 @@ class AddMeets extends Component {
                                 Add any meets or competitions that you've
                                 participated in.
                             </p>
-                            <div className="d-block pb-3">
+                            <small className="d-block pb-3">
                                 * = requred fields
-                            </div>
+                            </small>
+                            <form onSubmit={this.onSubmit}>
+                                <TextFieldGroup
+                                    placeholder="* Title"
+                                    name="title"
+                                    value={this.state.title}
+                                    onChange={this.onChange}
+                                    error={errors.title}
+                                />
+                                <TextFieldGroup
+                                    placeholder="* Federation"
+                                    name="federation"
+                                    value={this.state.federation}
+                                    onChange={this.onChange}
+                                    error={errors.federation}
+                                />
+                                <TextFieldGroup
+                                    placeholder="Location"
+                                    name="location"
+                                    value={this.state.location}
+                                    onChange={this.onChange}
+                                    error={errors.location}
+                                />
+                                <h6>Date Start</h6>
+                                <TextFieldGroup
+                                    name="datestart"
+                                    type="date"
+                                    value={this.state.datestart}
+                                    onChange={this.onChange}
+                                    error={errors.datestart}
+                                />
+                                <h6>Date End</h6>
+                                <TextFieldGroup
+                                    name="dateend"
+                                    type="date"
+                                    value={this.state.dateend}
+                                    onChange={this.onChange}
+                                    error={errors.dateend}
+                                />
+                                <TextAreaFieldGroup
+                                    placeholder="Description"
+                                    name="description"
+                                    value={this.state.description}
+                                    onChange={this.onChange}
+                                    error={errors.description}
+                                    info="Tell us about the this meet/ competition."
+                                />
+                                <TextFieldGroup
+                                    placeholder="Awards"
+                                    name="awards"
+                                    value={this.state.awards}
+                                    onChange={this.onChange}
+                                    error={errors.awards}
+                                />
+                                <input
+                                    type="submit"
+                                    value="Submit"
+                                    className="btn btn-info btn-block mt-4"
+                                />
+                            </form>
                         </div>
                     </div>
                 </div>
